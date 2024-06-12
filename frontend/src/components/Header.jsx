9876895f13f559519b9a8e08429b527b76439524
Praveen Ajjarapu <you@example.com>
2024-06-12 12:20:03 +0530

    added Sliding component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,15 @@ import SlidingText from './SlidingText'
 
 const Header = () => {
   return (
-    <div>
+    <div className='mx-2'>
       <SlidingText
         mainText={"Asset Brochure"}
         linkText={"Click here to download Rjy Asset Brochure 2024"}
         link={'#'}
       />
+      <div>
+        
+      </div>
     </div>
   );
 }
---
+++ frontend/src/constants.js
@@ -0,0 +1,15 @@
+// const headerTitles = [
+//     {text: 'Production'}
+// ]
+
+const headerList = [
+    {text: 'Production DPR', link: ''},
+    {text: 'Drilling DPR', link: ''},
+    {text: 'Well Services DPR', link: ''},
+    {text: 'Rjy Directory', link: ''},
+    {text: 'HSE-CIRCULARS', link: ''},
+    {text: 'Meeting Portal', link: ''},
+    {text: 'Complaint', link: ''},
+    {text: 'NAS Backup Storage Request', link: ''},
+    {text: 'PDF Tools', link: ''},
+]
