a34befc302dc585c3a31b6b2a4ad0ca37d5090a8
himanshumodi2001 <31august2001@gmail.com>
2024-06-13 16:07:19 +0530

    Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
-import { FaCopyright } from "react-icons/fa";
 import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
 
 
 function App() {
@@ -10,13 +10,15 @@ function App() {
 
   return (
     <div>
-     <Login />
-      {/* <Nav />
-      <Header />
+     {/*<Login />*/
+      <Home />
+     }
+      { /*<Nav />
+      <Header />*/
       <div className='flex py-2 items-center text-xl justify-center text-white bg-zinc-400 font-bold'>
-        <FaCopyright />
-        <p className="px-1">Designed By Infocom Services Rajahmundry</p>
-      </div> */}
+        <p className="px-1">&copy; Designed By Infocom Services Rajahmundry</p>
+      </div> 
+      }
     </div> 
   );
 }
