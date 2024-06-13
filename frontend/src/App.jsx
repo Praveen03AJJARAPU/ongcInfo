bfab89da8a778f92aa5b343ba69db217a8bedad2
Himanshu Kumar Modi <114941090+himanshumodi2001@users.noreply.github.com>
2024-06-13 11:55:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <div className='flex py-2 items-center text-xl justify-center text-white bg-zinc-400 font-bold'>
         <FaCopyright />
-        <p className="px-1">Designed By Infocom Services Rajahmundry</p>
+        <p className="px-1">&copy; Designed By Infocom Services Rajahmundry</p>
       </div>
     </div>
   );
