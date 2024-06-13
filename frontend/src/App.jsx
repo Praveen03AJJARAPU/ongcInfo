b293ddc2130a16bd853b26a191cf348656f27bd8
Divya Sri Satti <saiprudhvi2811@gmail.com>
2024-06-13 14:20:31 +0530

    loginpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,22 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
 import { FaCopyright } from "react-icons/fa";
+import Login from './pages/Login.jsx';
+
 
 function App() {
   
 
   return (
     <div>
-      <Nav />
+     <Login />
+      {/* <Nav />
       <Header />
       <div className='flex py-2 items-center text-xl justify-center text-white bg-zinc-400 font-bold'>
         <FaCopyright />
         <p className="px-1">Designed By Infocom Services Rajahmundry</p>
-      </div>
-    </div>
+      </div> */}
+    </div> 
   );
 }
 
